8cc73a521fc4a1bdb6048614781fc15a81ee6b59
Tanveer <tanvir3512@gmail.com>
2023-05-06 22:03:20 +0600

    sd

---
+++ src/Features/Checkout/Components/Checkout.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { useGlobalCtx } from '../../../Contexts/GlobalProvider';
+import Contact from './Contact'
+import Order from './Order'
+
+export function Checkout() {
+    const { register, handleSubmit } = useForm();
+    const { toggleModal, getPayment } = useGlobalCtx();
+    const onSubmit = (data) => {
+        // toggleModal();
+        getPayment(data)
+    };
+    
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid grid-cols-12 gap-x-8 py-12">
+                <div className="col-span-7">
+                    <Contact register={register} />
+                </div>
+                <div className="col-span-5 ">
+                    <Order />
+                </div>
+            </div>
+        </form>
+    )
+}
+export default Checkout;
---
+++ src/Hooks/useGlobal.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import countryCode from "../Features/Checkout/Data/countryCode.json";
+const useGlobal = () => {
+  const [open, setOpen] = useState(false);
+  const [mbCode, setMbCode] = useState(countryCode[15]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const toggleModal = () => setOpen(!open);
+
+  const getPayment = (body) => {
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/api/bkash/createPayment`, { ...body, totalPrice }, 
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        'content-type': 'application/json'
+      }
+    })
+    .then(res => {
+      const email = res.config.data?.email;
+      const paymentID = res.data;
+      axios.post(`${process.env.REACT_APP_SERVER_URL}/api/bkash/execute/?email=${email}&totalPrice=${totalPrice}&paymentID=${paymentID}`, {...body, totalPrice},
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          'content-type': 'application/json'
+        }
+      })
+      .then(res => console.log(res))
+
+    });
+   
+    
+  }
+  return {
+    toggleModal,
+    open,
+    setMbCode,
+    mbCode,
+    getPayment,
+    totalPrice,
+    setTotalPrice,
+  };
+};
+export default useGlobal;
